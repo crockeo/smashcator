9600827338f5f22b4310085a3bb291e89486f229
Cerek Hillen <crockeo@gmail.com>
2014-06-06 19:16:59 -0400

    Implemented part of the login page

--- Routes/Login.hs
+++ Routes/Login.hs
@@ -1,16 +1,33 @@
 module Routes.Login where
 
+import Control.Applicative ((<$>), (<*>))
+import qualified Data.Text as T
+
 import Yesod
 
 import Foundation
 import WidgetFile
 
+data LoginPost    = LoginPost    T.Text T.Text
+data RegisterPost = RegisterPost T.Text T.Text T.Text
+
 getLoginR :: Handler Html
 getLoginR =
   defaultLayout ($(widgetFile "login"))
 
-postLoginR :: Handler Html
-postLoginR = undefined
+postLoginR :: Handler ()
+postLoginR = do
+  (LoginPost username password) <- runInputPost $ LoginPost
+    <$> ireq textField     "username"
+    <*> ireq passwordField "password"
 
-postRegisterR :: Handler Html
-postRegisterR = undefined
+  redirect LoginR
+
+postRegisterR :: Handler ()
+postRegisterR = do
+  (RegisterPost username password cpassword) <- runInputPost $ RegisterPost
+    <$> ireq textField     "username"
+    <*> ireq passwordField "password"
+    <*> ireq passwordField "cpassword"
+
+  redirect LoginR
