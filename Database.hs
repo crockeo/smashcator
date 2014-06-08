1338229611205ba21deefd37cb6dcf3745c70929
Crockeo <crockeo@gmail.com>
2014-06-08 09:57:49 -0400

    Started implementing the database. I need to think about database schema.

---
+++ Database.hs
@@ -0,0 +1,12 @@
+module Database where
+
+import Database.Persistent.Sqilte
+import Database.Persistent.TH
+import Database.Persistent
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+User
+  username String
+  password Int
+|]
+
