ebd4aab085bc563952e0c191859c2cfa0dab4562
Cerek Hillen <crockeo@gmail.com>
2014-06-09 15:00:51 -0400

    Types were clashing, and to make it easier I just made the database use Data.Text instead of String

--- SmashDB.hs
+++ SmashDB.hs
@@ -11,26 +11,26 @@ import Data.Time.Clock
 -- Initializing the data schema
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 User
-  username String
+  username T.Text
   password Int
-  shortbio String  Maybe
-  longbio  String  Maybe
-  location String  Maybe
-  icon     String  Maybe
+  shortbio T.Text  Maybe
+  longbio  T.Text  Maybe
+  location T.Text  Maybe
+  icon     T.Text  Maybe
   signedup UTCTime default=CURRENT_TIME
 
 Tournament
-  title           String
+  title           T.Text
   host            UserId
   moderators      [UserId]
   attending       [UserId]
   invited         [UserId]
   public          Bool
-  description     String  Maybe
-  location        String  Maybe
+  description     T.Text  Maybe
+  location        T.Text  Maybe
   date            UTCTime Maybe
-  ruleset         String  Maybe
-  backgroundImage String  Maybe
+  ruleset         T.Text  Maybe
+  backgroundImage T.Text  Maybe
 |]
 
 -- Storing the database location
