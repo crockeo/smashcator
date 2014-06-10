a247a448640bd08dedae472868075eeea7cd3f94
Crockeo <crockeo@gmail.com>
2014-06-10 14:37:06 -0400

    Started implementing the tournament page.

--- Routes/Tournament.hs
+++ Routes/Tournament.hs
@@ -1,12 +1,18 @@
 module Routes.Tournament where
 
+import Database.Persist.Sqlite
+import Database.Persist
+
 import Yesod
 
 import Foundation
 import WidgetFile
+import SmashDB
 
 getTournamentR :: Int -> Handler Html
-getTournamentR id = undefined
+getTournamentR id = do
+  mtournament <- runSqlite dbLocation $ get $ Key $ PersistInt64 $ fromIntegral id
+  defaultLayout ($(widgetFile "tournament"))
 
 postNewTournamentR :: Handler Html
 postNewTournamentR = undefined
