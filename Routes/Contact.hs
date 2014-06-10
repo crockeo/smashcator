006fab4ccc5ef4a550f53b4a53a0d64fc57c4486
Cerek Hillen <crockeo@gmail.com>
2014-06-10 11:14:11 -0400

    Implemented the front-end of the contact page.

--- Routes/Contact.hs
+++ Routes/Contact.hs
@@ -1,10 +1,25 @@
 module Routes.Contact where
 
+import qualified Data.Text as T
+import Control.Applicative ((<$>), (<*>))
+
 import Yesod
 
 import Foundation
 import WidgetFile
 
+data ContactPost = ContactPost T.Text T.Text
+
 getContactR :: Handler Html
 getContactR =
   defaultLayout ($(widgetFile "contact"))
+
+postContactR :: Handler Html
+postContactR = do
+  (ContactPost subject message) <- runInputPost $ ContactPost
+    <$> ireq textField "subject"
+    <*> ireq textField "message"
+
+  -- TODO: Programmatically send email
+
+  redirect ContactR
