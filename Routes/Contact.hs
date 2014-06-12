d845bfcc2ebceb5b6847a1bca6005f62567d572b
Cerek Hillen <crockeo@gmail.com>
2014-06-12 13:46:27 -0400

    Implemented sending emails from the contact page; needs to be tested on a Unix system (because it was asking for /usr/sbin/sendmail on my Windows system

--- Routes/Contact.hs
+++ Routes/Contact.hs
@@ -1,7 +1,9 @@
 module Routes.Contact where
 
+import qualified Data.Text.Lazy as LT
 import qualified Data.Text as T
 import Control.Applicative ((<$>), (<*>))
+import Network.Mail.Mime
 
 import Yesod
 
@@ -20,6 +22,12 @@ postContactR = do
     <$> ireq textField "subject"
     <*> ireq textField "message"
 
-  -- TODO: Programmatically send email
+  liftIO $ (simpleMail
+    (Address (Just "Crockeo"   ) "crockeo@gmail.com")      -- Maintainer email
+    (Address (Just "Smashcator") "contact@smashcator.com") -- Website email
+    subject
+    (LT.pack $ T.unpack message)
+    ""
+    []) >>= renderSendMail
 
   redirect ContactR
