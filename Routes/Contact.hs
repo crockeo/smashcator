58b76d864dbe6f8e61185f5a795c41713526751e
Crockeo <crockeo@gmail.com>
2014-06-12 17:48:04 -0400

    Finally got emailing to work.

--- Routes/Contact.hs
+++ Routes/Contact.hs
@@ -1,8 +1,11 @@
 module Routes.Contact where
 
 import qualified Data.Text.Lazy as LT
+import Network.Mail.Client.Gmail
 import qualified Data.Text as T
 import Control.Applicative ((<$>), (<*>))
+import Control.Concurrent
+import Control.Exception (SomeException, try)
 import Network.Mail.Mime
 
 import Yesod
@@ -11,6 +14,19 @@ import Foundation
 import WidgetFile
 
 data ContactPost = ContactPost T.Text T.Text
+
+-- The login file location
+loginFile :: FilePath
+loginFile = "login"
+
+-- Loading the login information
+loadLogin :: FilePath -> IO (LT.Text, LT.Text)
+loadLogin fp = do
+  content <- readFile fp
+
+  case lines content of
+    (username:password:[]) -> return (LT.pack username, LT.pack password)
+    l                      -> return ("NULL", "NULL")
 
 getContactR :: Handler Html
 getContactR =
@@ -22,12 +38,17 @@ postContactR = do
     <$> ireq textField "subject"
     <*> ireq textField "message"
 
-  liftIO $ (simpleMail
-    (Address (Just "Crockeo"   ) "crockeo@gmail.com")      -- Maintainer email
-    (Address (Just "Smashcator") "contact@smashcator.com") -- Website email
-    subject
+  (username, password) <- liftIO $ loadLogin loginFile
+
+  liftIO $ sendGmail
+    username
+    password
+    (Address (Just "Crockeo") "crockeo@gmail.com")
+    [Address (Just "Crockeo") "crockeo@gmail.com"]
+    []
+    []
+    ("Smashcator Contact - " `T.append` subject)
     (LT.pack $ T.unpack message)
-    ""
-    []) >>= renderSendMail
+    []
 
   redirect ContactR
