8f556fc70b5548bfc24ecdfc8613d5ed4db8b81a
Cerek Hillen <crockeo@gmail.com>
2014-06-13 15:02:10 -0400

    Added some basic imeplementation of the EditUser page.

--- Routes/EditUser.hs
+++ Routes/EditUser.hs
@@ -3,7 +3,14 @@ module Routes.EditUser where
 import Import
 
 getEditUserR :: Text -> Handler Html
-getEditUserR t = undefined
+getEditUserR username = do
+  pusers <- runSqlite dbLocation $ selectList [UserUsername ==. username] []
+  mloggedin <- lookupSession "loggedin"
+
+  let muser = if null pusers
+                then Nothing
+                else Just $ head pusers in
+    defaultLayout ($(widgetFile "edituser"))
 
 postEditUserR :: Text -> Handler ()
 postEditUserR t = redirect $ EditUserR t
