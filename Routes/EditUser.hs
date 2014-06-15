5978ac82cd867ab1820ead121524cef18bfac571
Crockeo <crockeo@gmail.com>
2014-06-15 18:25:15 -0400

    Implemented the edit user page

--- Routes/EditUser.hs
+++ Routes/EditUser.hs
@@ -2,15 +2,46 @@ module Routes.EditUser where
 
 import Import
 
+data EditUserPost = EditUserPost (Maybe Text) (Maybe Text) (Maybe Text) (Maybe Text) (Maybe Text) (Maybe Text) Text
+
 getEditUserR :: Text -> Handler Html
 getEditUserR username = do
-  pusers <- runSqlite dbLocation $ selectList [UserUsername ==. username] []
+  muser <- runSqlite dbLocation $ selectFirst [UserUsername ==. username] []
   mloggedin <- lookupSession "loggedin"
 
-  let muser = if null pusers
-                then Nothing
-                else Just $ head pusers in
-    defaultLayout ($(widgetFile "edituser"))
+  defaultLayout ($(widgetFile "edituser"))
 
 postEditUserR :: Text -> Handler ()
-postEditUserR t = redirect $ EditUserR t
+postEditUserR username = do
+  (EditUserPost mnewpassword mcnewpassword micon mshortbio mlocation mlongbio password) <- runInputPost $ EditUserPost
+    <$> iopt passwordField "newpassword"
+    <*> iopt passwordField "cnewpassword"
+    <*> iopt textField     "icon"
+    <*> iopt textField     "shortbio"
+    <*> iopt textField     "location"
+    <*> iopt textField     "longbio"
+    <*> ireq passwordField "password"
+
+  muser <- runSqlite dbLocation $ selectFirst [UserUsername ==. username, UserPassword ==. hash password] []
+
+  case muser of
+    Nothing                   -> do
+      setMessage "Invalid password."
+      redirect $ EditUserR username
+    Just (Entity userId user) -> do
+      runSqlite dbLocation $ do
+        case mnewpassword of 
+          Nothing          -> return ()
+          Just newpassword ->
+            case mcnewpassword of
+              Nothing -> return ()
+              Just cnewpassword ->
+                if newpassword == cnewpassword
+                  then update userId [UserPassword =. hash newpassword]
+                  else return ()
+
+        update userId [UserIcon     =. micon    ]
+        update userId [UserShortbio =. mshortbio]
+        update userId [UserLocation =. mlocation]
+        update userId [UserLongbio  =. mlongbio ]
+      redirect $ UserR username
