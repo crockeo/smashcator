322f6d8cca42006603d6bf87524efaf688d3ea36
Crockeo <crockeo@gmail.com>
2014-06-19 15:44:46 -0400

    Added a function to get the Int id from a Key Tournament

--- Routes/Tournaments.hs
+++ Routes/Tournaments.hs
@@ -1,6 +1,14 @@
 module Routes.Tournaments where
 
+import Data.Int
+
 import Import
+
+makeId :: Key Tournament -> Int
+makeId key =
+  case fromPersistValue $ unKey key :: Either Text Int64 of
+    Left  _ -> 0
+    Right v -> fromIntegral v
 
 getTournamentsR :: Handler Html
 getTournamentsR = do
