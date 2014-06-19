2cb746c3b809aa09941223f69b0e7b9a6d51b9df
Cerek Hillen <crockeo@gmail.com>
2014-06-19 01:05:47 -0400

    Been trying to implement generating the form from Yesod code. Moved the old form hamlet file to a different file.

--- Routes/Tournament.hs
+++ Routes/Tournament.hs
@@ -2,14 +2,46 @@ module Routes.Tournament where
 
 import Import
 
+data NewTournament = NewTournament
+  { public          :: Bool
+  , title           :: Text
+  , location        :: Text
+  , date            :: Text
+  , backgroundImage :: (Maybe Text)
+  , games           :: [Text]
+  , description     :: Textarea
+  , ruleset         :: Textarea }
+  deriving (Show)
+
+newTournamentForm :: Html -> MForm Handler (FormResult NewTournament, Widget)
+newTournamentForm = renderBootstrap3 BootstrapBasicForm $ NewTournament
+  <$> areq checkBoxField                   "Public"                                                                         Nothing
+  <*> areq textField                       (withPlaceholder "Enter title"                $ bfs ("Title"           :: Text)) Nothing
+  <*> areq textField                       (withPlaceholder "Enter location"             $ bfs ("Location"        :: Text)) Nothing
+  <*> areq textField                       (withPlaceholder "Enter date"                 $ bfs ("Date"            :: Text)) Nothing
+  <*> aopt textField                       (withPlaceholder "Enter background image url" $ bfs ("BackgroundImage" :: Text)) Nothing
+  <*> areq (checkboxesFieldList gamesList) "Games:"                                                                         Nothing
+  <*> areq textareaField                   (withPlaceholder "Enter description"          $ bfs ("Description"     :: Text)) Nothing
+  <*> areq textareaField                   (withPlaceholder "Enter ruleset"              $ bfs ("Ruleset"         :: Text)) Nothing
+  where gamesList :: [(Text, Text)]
+        gamesList = [ ("Super Smash Bros. 64"   , "64"   )
+                    , ("Super Smash Bros. Melee", "melee")
+                    , ("Super Smash Bros. Brawl", "brawl")
+                    , ("Project M"              , "pm"   )
+                    ]
+
 getTournamentR :: Int -> Handler Html
 getTournamentR id = do
   mtournament <- runSqlite dbLocation $ get $ Key $ PersistInt64 $ fromIntegral id
   defaultLayout ($(widgetFile "tournament"))
 
 getNewTournamentR :: Handler Html
-getNewTournamentR =
+getNewTournamentR = do
+  (form, enctype) <- generateFormPost newTournamentForm
   defaultLayout ($(widgetFile "newtournament"))
 
 postNewTournamentR :: Handler ()
-postNewTournamentR = redirect NewTournamentR
+postNewTournamentR = do
+  ((nt, _), _) <- runFormPost newTournamentForm
+  liftIO $ print nt
+  redirect NewTournamentR
