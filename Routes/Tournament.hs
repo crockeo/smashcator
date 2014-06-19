df327ae89f99b6f9a3354a3debf7b7bb1cda96f0
Crockeo <crockeo@gmail.com>
2014-06-19 14:48:27 -0400

    Added functionality to the NewTournament screen.

--- Routes/Tournament.hs
+++ Routes/Tournament.hs
@@ -1,16 +1,18 @@
 module Routes.Tournament where
 
+import qualified Data.Text as T hiding (Text)
+
 import Import
 
-data NewTournament = NewTournament Bool Text Text Text (Maybe Text) [Text] Textarea Textarea
+data NewTournament = NewTournament Bool Text (Maybe Text) (Maybe Text) (Maybe Text) [Text] Textarea Textarea
   deriving (Show)
 
 newTournamentForm :: Html -> MForm Handler (FormResult NewTournament, Widget)
 newTournamentForm = renderBootstrap3 BootstrapBasicForm $ NewTournament
   <$> areq checkBoxField                   "Public"                                                                         Nothing
   <*> areq textField                       (withPlaceholder "Enter title"                $ bfs ("Title"           :: Text)) Nothing
-  <*> areq textField                       (withPlaceholder "Enter location"             $ bfs ("Location"        :: Text)) Nothing
-  <*> areq textField                       (withPlaceholder "Enter date"                 $ bfs ("Date"            :: Text)) Nothing
+  <*> aopt textField                       (withPlaceholder "Enter location"             $ bfs ("Location"        :: Text)) Nothing
+  <*> aopt textField                       (withPlaceholder "Enter date"                 $ bfs ("Date"            :: Text)) Nothing
   <*> aopt textField                       (withPlaceholder "Enter background image url" $ bfs ("BackgroundImage" :: Text)) Nothing
   <*> areq (checkboxesFieldList gamesList) "Games:"                                                                         Nothing
   <*> areq textareaField                   (withPlaceholder "Enter description"          $ bfs ("Description"     :: Text)) Nothing
@@ -30,10 +32,36 @@ getTournamentR id = do
 getNewTournamentR :: Handler Html
 getNewTournamentR = do
   (form, enctype) <- generateFormPost newTournamentForm
+  loggedin        <- lookupSession "loggedin"
   defaultLayout ($(widgetFile "newtournament"))
 
 postNewTournamentR :: Handler ()
 postNewTournamentR = do
-  ((nt, _), _) <- runFormPost newTournamentForm
-  liftIO $ print nt
-  redirect NewTournamentR
+  ((result, _), _) <- runFormPost newTournamentForm
+  mloggedin        <- lookupSession "loggedin"
+  muser            <-
+    case mloggedin of
+      Nothing       -> return Nothing
+      Just loggedin -> runSqlite dbLocation $ getBy $ UniqueName loggedin
+
+  case muser of
+    Nothing -> setMessage "You are not logged in as a valid user." >> redirect NewTournamentR
+    Just (Entity userId user) ->
+      case result of
+        FormFailure _ -> setMessage "Could not validate form." >> redirect NewTournamentR
+        FormSuccess (NewTournament public title location date backgroundImage games description ruleset) -> do
+          runSqlite dbLocation $
+            insert $ Tournament { tournamentTitle           = title
+                                , tournamentLocation        = location
+                                , tournamentDate            = fmap (fst . head . reads . T.unpack) date
+                                , tournamentBackgroundImage = backgroundImage
+                                , tournamentGames           = games
+                                , tournamentDescription     = unTextarea description
+                                , tournamentRuleset         = unTextarea ruleset
+                                , tournamentHost            = userId
+                                , tournamentModerators      = []
+                                , tournamentAttending       = []
+                                , tournamentInvited         = []
+                                , tournamentPublic          = public
+                                }
+          redirect TournamentsR
